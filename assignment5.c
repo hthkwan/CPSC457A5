56631c0312bc6346a306faeedb89fe2ea35b967a
Jeremy Luzzi <jluzz403@gmail.com>
2016-04-12 11:50:05 -0600

    fixed all compilation errors

--- assignment5.c
+++ assignment5.c
@@ -17,13 +17,13 @@ typedef struct queue{
 	int waiting;
 } prod_cons_queue;
 
-struct vars{
+typedef struct vars{
 	prod_cons_queue *q;
 	int pid;
-};
+} vars;
 
 int main(){
-	
+
 	return 0;
 }
 
@@ -36,16 +36,14 @@ void queue_initialize (prod_cons_queue *q){
 	q -> waiting = 0;
 }
 
-void queue_add( prod_cons_queue *q, int element);
-int queue_remove( prod_cons_queue *q);
-
 void queue_add( prod_cons_queue *q, int element){
-	if(q->remaining_elements == 20){	
-		if(tail < 19){
+	if(q->remaining_elements == 20){
+		//is this supposed to be remaining_elements or tail?
+		if(q->tail < 19){
 			q->element[q->tail + 1] = element;
 			q->tail ++;
 		}else{
-			q->element[0] = this.element;
+			q->element[0] = element;
 			q->tail = 0;
 		}
 		q->remaining_elements ++;
@@ -53,48 +51,44 @@ void queue_add( prod_cons_queue *q, int element){
 
 }
 
-
 void queue_remove( prod_cons_queue *q){
 	if(q->remaining_elements != 0){
 		if(q->head < 19){
 			q->head += 1;
-			remaining_elements -= 1;
+			q->remaining_elements -= 1;
 		}else{
 			q->head = 0;
-			remaining_elements -= 1;
+			q->remaining_elements -= 1;
 		}
-		
+
 	}
 }
 
 void* producer(void *ptr){
 	int msgs = 10;
-	for(int i = 0;i<msgs;i++){	
+	for(int i = 0;i<msgs;i++){
 		pthread_mutex_lock(&mutex);
-		if(prod_cons_queue -> remaining_elements == 20){
-			prod_cons_queue -> waiting += 1;
+		if(((vars *)ptr)->q->remaining_elements == 20){
+			((vars *)ptr)->q->waiting += 1;
 			pthread_cond_wait(&condp, &mutex);
 		}
-		prod_cons_queue -> waiting -= 1;
-		queue_add(prod_cons_queue, (int)ptr);
-		if(prod_cons_queue -> remaining_elements == 1)pthread_cond_signal(&condc);
+		((vars *)ptr)->q->waiting -= 1;
+		queue_add(((vars *)ptr)->q, ((vars *)ptr)->pid);
+		if(((vars *)ptr)->q->remaining_elements == 1)pthread_cond_signal(&condc);
 		pthread_mutex_unlock(&mutex);
 	}
 }
 
 void* consumer(void *ptr){
-	struct vars * args = (vars)ptr;
 	int msgs = 0;
-	if(prod_cons_queue ->remaining_elements == 0){
-		prod_cons_queue -> waiting += 1;
+	if(((vars *)ptr)->q->remaining_elements == 0){
+		((vars *)ptr)->q->waiting += 1;
 		pthread_cond_wait(&condc, &mutex);
 	}
-	prod_cons_queue -> waiting -= 1;
+	((vars *)ptr)->q->waiting -= 1;
 	//there are messages in the queue now
-	int pid = prod_cons_queue -> element[prod_cons_queue -> head];
-	printf("This is a message from thread %d", pid);
-	queue_remove(prod_cons_queue);
-	if(prod_cons_queue -> remaining_elements == 19) pthread_cond_signal(&condp);
+	int pid = ((vars *)ptr)->q->element[((vars *)ptr)->q->head];
+	printf("This is a message from thread %d", ((vars *)ptr)->pid);
+	queue_remove(((vars *)ptr)->q);
+	if(((vars *)ptr)->q->remaining_elements == 19) pthread_cond_signal(&condp);
 }
-
-
