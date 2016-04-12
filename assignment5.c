6f3986c928b07a037792846695ef9c1a85e2b676
hthkwan <herman_fire@hotmail.com>
2016-04-11 19:15:20 -0600

    What we've done in tutorial

--- assignment5.c
+++ assignment5.c
@@ -1,8 +1,7 @@
-;; This buffer is for notes you don't want to save, and for Lisp evaluation.
-;; If you want to create a file, visit that file with C-x C-f,
-;; then enter the text in that file's own buffer.
+//Remember to change "prod_cons_queue" to the actual name of the queue
 
 #include <pthread.h>
+#include <stdint.h>
 
 #define MAX_QUEUE_SIZE 20
 
@@ -15,7 +14,13 @@ typedef struct queue{
 	uint8_t tail;
 	uint8_t remaining_elements;
 	//additional var here
+	int waiting;
 } prod_cons_queue;
+
+struct vars{
+	prod_cons_queue *q;
+	int pid;
+};
 
 int main(){
 	
@@ -23,11 +28,12 @@ int main(){
 }
 
 void queue_initialize (prod_cons_queue *q){
-	for(int = 0;i< 20;i++){
-		element[i] = null;
+	for(int i = 0;i< 20;i++){
+		q -> element[i] = NULL;
 	}
-	head = tail = 0;
-	remaining_elements = 0;
+	(q -> head) = (q -> tail) = 0;
+	q -> remaining_elements = 0;
+	q -> waiting = 0;
 }
 
 void queue_add( prod_cons_queue *q, int element);
@@ -65,7 +71,11 @@ void* producer(void *ptr){
 	int msgs = 10;
 	for(int i = 0;i<msgs;i++){	
 		pthread_mutex_lock(&mutex);
-		if(prod_cons_queue -> remaining_elements == 20) pthread_cond_wait(&condp, &mutex);
+		if(prod_cons_queue -> remaining_elements == 20){
+			prod_cons_queue -> waiting += 1;
+			pthread_cond_wait(&condp, &mutex);
+		}
+		prod_cons_queue -> waiting -= 1;
 		queue_add(prod_cons_queue, (int)ptr);
 		if(prod_cons_queue -> remaining_elements == 1)pthread_cond_signal(&condc);
 		pthread_mutex_unlock(&mutex);
@@ -73,8 +83,13 @@ void* producer(void *ptr){
 }
 
 void* consumer(void *ptr){
+	struct vars * args = (vars)ptr;
 	int msgs = 0;
-	if(prod_cons_queue ->remaining_elements == 0) pthread_cond_wait(&condc, &mutex);
+	if(prod_cons_queue ->remaining_elements == 0){
+		prod_cons_queue -> waiting += 1;
+		pthread_cond_wait(&condc, &mutex);
+	}
+	prod_cons_queue -> waiting -= 1;
 	//there are messages in the queue now
 	int pid = prod_cons_queue -> element[prod_cons_queue -> head];
 	printf("This is a message from thread %d", pid);
